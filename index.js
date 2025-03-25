7e7aef21f74ac6b823890c19a21b0417f77fa67b
danier00 <danieralvarez090@gmail.com>
2025-03-25 08:51:16 -0500

    Complet

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express"); //Requiriendo express
+const app = express(); //Creando app con constructor express
+const port = 3005; //puerto:usualmente es por el orden del 3000
+//obteniendo de la aplicación la ruta y un creando callback
+//conmensaje indicando mi servidir
+app.get("/", (req, res) => {
+  //Parámetro 1 es la ruta, el 2 un callback
+  res.send("Este Es El Server De Danier");
+});
+//Indicando que debe escuchar el puerto 3005
+app.listen(port, () => {
+  //Parámetro 1 es el puerto, el 2 un callback
+  console.log("Escuchando el puerto: " + port);
+});
