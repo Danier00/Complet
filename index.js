66337a4696a7c4bf6a570a3f2e18e7e1c92d4b22
danier00 <danieralvarez090@gmail.com>
2025-03-25 10:19:00 -0500

    hola

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.get("/", (req, res) => {
   //Parámetro 1 es la ruta, el 2 un callback
   res.send("Este Es El Server De Danier");
 });
+app.get("/nuevo", (req, res) => {
+  //Parámetro 1 es la ruta, el 2 un callback
+  res.send("Esta Es La Nueva Ruta");
+});
+app.get("/productos", (req, res) => {
+  //Parámetro 1 es la ruta, el 2 un callback
+  res.json({name:'XXXXXX',precio:12000});
+});
 //Indicando que debe escuchar el puerto 3005
 app.listen(port, () => {
   //Parámetro 1 es el puerto, el 2 un callback
