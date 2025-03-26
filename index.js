a456aaeffd65984dc874fd8dd2e8e3f81324345e
danier00 <danieralvarez090@gmail.com>
2025-03-26 08:58:31 -0500

    hola

--- index.js
+++ index.js
@@ -13,8 +13,69 @@ app.get("/nuevo", (req, res) => {
 });
 app.get("/productos", (req, res) => {
   //Parámetro 1 es la ruta, el 2 un callback
-  res.json({name:'XXXXXX',precio:12000});
+  res.json([
+    { name: "XXXXXX", precio: 12000 },
+    { name: "Producto2", precio: 1000 },
+  ]);
 });
+
+app.get("/productos/:id", (req, res) => {
+  //Parámetro 1 es la ruta, el 2 un callback
+  const id = req.params.id;
+  res.json([{ id, name: "XXXXXX", precio: 12000 }]);
+});
+
+app.get("/categorias/:cateId/productos/:prodId", (req, res) => {
+  //Parámetro 1 es la ruta, el 2 un callback
+  const { cateId, prodId } = req.params;
+  res.json([{ cateId, prodId }]);
+});
+
+// app.get("/usuarios", (req, res) => {
+//   //Parámetro 1 es la ruta, el 2 un callback
+//   res.json([
+//     { name: "Danier", precio: 20 },
+//     { name: "XXXXXXXX", precio: 19 },
+//   ]);
+// });
+
+app.get("/usuarios/:id", (req, res) => {
+  const {id}=req.params
+  //Parámetro 1 es la ruta, el 2 un callback
+  res.json([
+    { id, name: "Danier", precio: 20 },
+  ]);
+});
+
+app.get("/usuarios", (req, res) => {
+  const {limit,offset}=req.query
+  //Parámetro 1 es la ruta, el 2 un callback
+
+  if(limit&&offset){
+  res.json([
+    { limit,offset},
+  ]);}
+  else{
+    res.send('No hay parametros')
+  }
+});
+
+app.get("/categorias", (req, res) => {
+  //Parámetro 1 es la ruta, el 2 un callback
+  res.json([
+    { name: "Granos" },
+    { name: "Aseo"  },
+  ]);
+});
+
+app.get("/categorias/:id", (req, res) => {
+  const{id}=req.params
+  //Parámetro 1 es la ruta, el 2 un callback
+  res.json([
+    { id,name: "Granos" }
+  ]);
+});
+
 //Indicando que debe escuchar el puerto 3005
 app.listen(port, () => {
   //Parámetro 1 es el puerto, el 2 un callback
